c1550c848aeb544581866e7da4ffdad7f9669883
samdaphbynet <lj8@hotmail.fr>
2023-08-23 11:45:46 +0200

    app that start flask application

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    This is a Flask application that registers blueprints for API views and sets up a teardown function
+    to close the storage context after the application context is torn down.
+"""
+from flask import Flask
+import os
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_app_context(exception):
+    """
+    The above function is a decorator that is used to close the storage context after the application
+    context is torn down.
+    """
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
