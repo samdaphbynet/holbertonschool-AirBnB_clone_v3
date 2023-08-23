7b08c44c6464113944de25491b2b85bae49166af
samdaphbynet <lj8@hotmail.fr>
2023-08-23 11:45:24 +0200

    init files

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,5 @@
+# This code is creating a Flask Blueprint object named `app_views`. A Blueprint is a way to organize
+# related routes and views in a Flask application.
+from flask import Blueprint
+app_views = Blueprint("/api/v1", __name__, url_prefix="/api/v1")
+from api.v1.views.index import *
