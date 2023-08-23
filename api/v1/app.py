412f32f3c84762fbddd98da1355f0dcae912dd50
samdaphbynet <lj8@hotmail.fr>
2023-08-23 12:01:12 +0200

    fixed pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-    This is a Flask application that registers blueprints for API views and sets up a teardown function
+    This is a Flask application that registers blueprints for API views
+    and sets up a teardown function
     to close the storage context after the application context is torn down.
 """
 from flask import Flask
@@ -8,17 +9,21 @@ import os
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_app_context(exception):
     """
-    The above function is a decorator that is used to close the storage context after the application
+    The above function is a decorator that is used to close the storage
+    context after the application
     context is torn down.
     """
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
